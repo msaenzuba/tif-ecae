5fc50c1ce78d23704cde5e9edbd59ecf074c342e
Martín Horacio José Sáenz Valiente <msaenz@sociales.uba.ar>
2026-05-22 15:23:49 -0300

    Update b-scrapeolinkstw.py

--- b-scrapeolinkstw.py
+++ b-scrapeolinkstw.py
@@ -2,7 +2,7 @@ import pandas as pd
 from bs4 import BeautifulSoup
 import re
 
-file_path = r"c:\Users\marti\Documents\Facu\Beca Maestría\Scraping\código_fuenteinmigracion rusa AND argentina.html"
+file_path = r"______"
 with open(file_path, 'r', encoding='utf-8') as file:
     content = file.read()
 soup = BeautifulSoup(content, 'html.parser') # Parsear el HTML con BeautifulSoup
@@ -19,5 +19,5 @@ df = pd.DataFrame(tweet_links, columns=['url']) # Crear un DataFrame de pandas c
 print(df) # Mostrar el DataFrame
 df = df.drop_duplicates() # Eliminar duplicados
 # Guardar los links en un archivo CSV
-output_path = 'C:\\Users\\marti\\Documents\\Facu\\Beca Maestría\\Scraping\\Selenium\\linkstweets_inmigracion{terminos_busqueda}.csv'
+output_path = 'C:______{terminos_busqueda}.csv'
 df.to_csv(output_path, index=False)
